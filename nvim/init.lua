e539796b44704388e143f50406e6d73d3f040876
ovier-oh <ovier.obregon.hin@outlook.com>
2026-03-03 01:56:32 +0200

    Add LSP, snippets, markdown support and telescope

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,10 +1,7 @@
--- Settings runtimepath pfor lazy.nvim 
+-- Settings runtimepath pfor lazy.nvim
 
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
-
-
-
 
 vim.opt.rtp:prepend("~/.local/share/nvim/lazy/lazy.nvim")
 
@@ -18,63 +15,222 @@ require("lazy").setup({
     {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
-        lazy = false,  -- 👈 importante
+        lazy = false, -- 👈 importante
         priority = 1000,
         config = function()
-        local ok, configs = pcall(require, "nvim-treesitter.configs")
+            local ok, configs = pcall(require, "nvim-treesitter.configs")
             if not ok then
-              return
+                return
             end
 
             configs.setup({
-              ensure_installed = { "lua", "python", "bash" },
-              highlight = { enable = true },
+                ensure_installed = { "lua", "python", "bash", "markdown", "markdown_inline" },
+                highlight = { enable = true },
             })
         end,
     },
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end,
+    },
+
+    {
+        "williamboman/mason-lspconfig.nvim",
+        dependencies = { "mason.nvim" },
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = { "pyright", "clangd", "lua_ls" },
+            })
+        end,
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            -- Activar servidores
+            vim.lsp.enable("pyright")
+            vim.lsp.enable("clangd")
+
+            -- Formateo al guardar
+            vim.api.nvim_create_autocmd("LspAttach", {
+                callback = function(args)
+                    local bufnr = args.buf
+
+                    vim.api.nvim_create_autocmd("BufWritePre", {
+                        buffer = bufnr,
+                        callback = function()
+                            vim.lsp.buf.format({ bufnr = bufnr })
+                        end,
+                    })
+                end,
+            })
+        end,
+    },
+    {
+        "nvim-telescope/telescope.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        config = function()
+            require("telescope").setup({})
+        end,
+    },
+    {
+        "vim-airline/vim-airline",
+        dependencies = { "vim-airline/vim-airline-themes" },
+    },
+    {
+        "L3MON4D3/LuaSnip",
+        dependencies = { "rafamadriz/friendly-snippets" },
+        build = "make install_jsregexp",
+        config = function()
+            require("luasnip.loaders.from_vscode").lazy_load()
+        end,
+    },
+
+    {
+        "hrsh7th/nvim-cmp",
+        dependencies = {
+            "saadparwaiz1/cmp_luasnip",
+            "hrsh7th/cmp-nvim-lsp",
+            "L3MON4D3/LuaSnip",
+        },
+        config = function()
+            local cmp = require("cmp")
+            local luasnip = require("luasnip")
+
+            cmp.setup({
+                snippet = {
+                    expand = function(args)
+                        luasnip.lsp_expand(args.body)
+                    end,
+                },
+                mapping = cmp.mapping.preset.insert({
+                    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+                    ["<Tab>"] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_next_item()
+                        elseif luasnip.expand_or_jumpable() then
+                            luasnip.expand_or_jump()
+                        else
+                            fallback()
+                        end
+                    end, { "i", "s" }),
+                    ["<S-Tab>"] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_prev_item()
+                        elseif luasnip.jumpable(-1) then
+                            luasnip.jump(-1)
+                        else
+                            fallback()
+                        end
+                    end, { "i", "s" }),
+                }),
+                sources = {
+                    { name = "nvim_lsp" },
+                    { name = "luasnip" },
+                },
+            })
+        end,
+    },
+    {
+        "iamcco/markdown-preview.nvim",
+        build = "cd app && npm install",
+        ft = { "markdown" },
+        config = function()
+            vim.g.mkdp_auto_start = 0
+            vim.g.mkdp_auto_close = 1
+        end,
+    },
+
 })
 
 
 -- ======================== Basic Options =============
-vim.opt.number = true 
-vim.opt.relativenumber = true 
-vim.opt.cursorline = true 
-vim.opt.showmatch = true 
-vim.opt.incsearch = true 
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.cursorline = true
+vim.opt.showmatch = true
+vim.opt.incsearch = true
 vim.opt.hlsearch = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4 
-vim.opt.expandtab = true 
-vim.opt.autoindent = true 
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.autoindent = true
 vim.opt.smartindent = true
 vim.opt.wrap = true
-vim.opt.linebreak = true 
+vim.opt.linebreak = true
 vim.opt.mouse = "a"
 vim.opt.clipboard = "unnamedplus"
-vim.opt.termguicolors = true 
+vim.opt.termguicolors = true
 
-vim.cmd("syntax on") 
-vim.cmd("filetype plugin indent on") 
+vim.cmd("syntax on")
+vim.cmd("filetype plugin indent on")
 
 
 vim.cmd("colorscheme gruvbox")
 vim.opt.background = "dark"
 
-local keymap = vim.keymap 
+local keymap = vim.keymap
 
 
 
 keymap.set("n", "<C-n>", ":NERDTreeToggle<CR>", { silent = true })
-keymap.set("n", "<C-s>", ":w<CR>", { silent = true })
+
 keymap.set("n", "<leader>q", ":q<CR>", { silent = true })
+keymap.set("n", "<C-q>", ":q!<CR>", { silent = true })
+keymap.set("n", "<leader>w", ":w<CR>", { silent = true })
 keymap.set("n", "<leader>x", ":x<CR>", { silent = true })
 keymap.set("i", "jk", "<Esc>", { silent = true })
 
 
 
 
-keymap.set("n","<leader>h", ":nohlsearch<CR>", { silent = true })
+keymap.set("n", "<leader>h", ":nohlsearch<CR>", { silent = true })
+
+vim.api.nvim_create_autocmd("LspAttach", {
+    callback = function(args)
+        local opts = { buffer = args.buf, silent = true }
+
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+        vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+        vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+    end,
+})
 
 
+local builtin = require("telescope.builtin")
+
+vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+
+
+vim.diagnostic.config({
+    virtual_text = false, -- menos ruido inline
+    signs = true,
+    underline = true,
+    float = {
+        border = "rounded",
+    },
+})
+
+vim.api.nvim_create_autocmd("CursorHold", {
+    callback = function()
+        vim.diagnostic.open_float(nil, { focusable = false })
+    end,
+})
+
+vim.g.airline_theme = "dark"
+
+
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "markdown",
+    callback = function()
+        vim.keymap.set("n", "<F3>", ":MarkdownPreview<CR>", { buffer = true, silent = true })
+    end,
+})
